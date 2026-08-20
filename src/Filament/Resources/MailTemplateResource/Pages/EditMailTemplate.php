bfa08197b7015c46212daff00acd08b738ebc04f
Jyrki De Neve <jyrki.deneve@gmail.com>
2026-08-20 19:27:46 +0300

    Add support for translatable-tabs v3 (#36)
    * Add support for translatable-tabs v3
    
    * Drop redundant getCancelFormAction override

--- src/Filament/Resources/MailTemplateResource/Pages/EditMailTemplate.php
+++ src/Filament/Resources/MailTemplateResource/Pages/EditMailTemplate.php
@@ -4,12 +4,9 @@ namespace Wotz\FilamentMailTemplates\Filament\Resources\MailTemplateResource\Pag
 
 use Filament\Resources\Pages\EditRecord;
 use Wotz\FilamentMailTemplates\Filament\Resources\MailTemplateResource;
-use Wotz\TranslatableTabs\Resources\Traits\HasTranslations;
 
 class EditMailTemplate extends EditRecord
 {
-    use HasTranslations;
-
     protected static string $resource = MailTemplateResource::class;
 
     protected function getActions(): array
