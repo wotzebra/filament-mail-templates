690d2dc4c1347effe95f2a1d0296ac4a83c094e0
Jyrki De Neve <jyrki.deneve@gmail.com>
2026-08-20 16:47:08 +0300

    Add support for translatable-tabs v3

--- src/Filament/Resources/MailTemplateResource/Pages/EditMailTemplate.php
+++ src/Filament/Resources/MailTemplateResource/Pages/EditMailTemplate.php
@@ -2,18 +2,25 @@
 
 namespace Wotz\FilamentMailTemplates\Filament\Resources\MailTemplateResource\Pages;
 
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Js;
 use Wotz\FilamentMailTemplates\Filament\Resources\MailTemplateResource;
-use Wotz\TranslatableTabs\Resources\Traits\HasTranslations;
 
 class EditMailTemplate extends EditRecord
 {
-    use HasTranslations;
-
     protected static string $resource = MailTemplateResource::class;
 
     protected function getActions(): array
     {
         return [];
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return Action::make('cancel')
+            ->label(__('filament-panels::resources/pages/edit-record.form.actions.cancel.label'))
+            ->alpineClickHandler('(window.location.href = ' . Js::from($this->previousUrl ?? static::getResource()::getUrl()) . ')')
+            ->color('gray');
     }
 }
