f9b9ca49ea44d6c0b78a9512d5dde11bfbaa2096
Jyrki De Neve <jyrki.deneve@gmail.com>
2026-08-20 18:12:20 +0300

    Drop redundant getCancelFormAction override

--- src/Filament/Resources/MailTemplateResource/Pages/EditMailTemplate.php
+++ src/Filament/Resources/MailTemplateResource/Pages/EditMailTemplate.php
@@ -2,9 +2,7 @@
 
 namespace Wotz\FilamentMailTemplates\Filament\Resources\MailTemplateResource\Pages;
 
-use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
-use Illuminate\Support\Js;
 use Wotz\FilamentMailTemplates\Filament\Resources\MailTemplateResource;
 
 class EditMailTemplate extends EditRecord
@@ -14,13 +12,5 @@ class EditMailTemplate extends EditRecord
     protected function getActions(): array
     {
         return [];
-    }
-
-    protected function getCancelFormAction(): Action
-    {
-        return Action::make('cancel')
-            ->label(__('filament-panels::resources/pages/edit-record.form.actions.cancel.label'))
-            ->alpineClickHandler('(window.location.href = ' . Js::from($this->previousUrl ?? static::getResource()::getUrl()) . ')')
-            ->color('gray');
     }
 }
